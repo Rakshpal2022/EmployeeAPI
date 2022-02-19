235778e62eba92798bd78ff95ca14601792a4406
Rakshpal2022 <abhysinghlko@gmail.com>
2022-02-19 13:24:59 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+class Employee<T>
+{
+public void GetTransaction(T i){
+    Console.WriteLine(i);
+}
+}
+class HCL:Employee<double>{
+public static void Main(){
+  HCL obj=new HCL();
+    obj.GetTransaction(10.5);
+}
+}
